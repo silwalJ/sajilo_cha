4947cca49fce06603aa946d568f889919c780e52
Jaya Silwal <silwaljaya@gmail.com>
2022-11-12 08:06:03 +0545

    fifth commit

--- sajilo/config/common.py
+++ sajilo/config/common.py
@@ -74,6 +74,12 @@ class Common(Configuration):
         )
     }
 
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
     # General
     APPEND_SLASH = False
     TIME_ZONE = 'UTC'
